485a3d2d410848657f5af63d40054d765ef95501
Adrian-palacios19 <adrianspc1@gmail.com>
2022-08-09 18:24:38 -0500

    cambio en el primer ejercicio de la rama main :)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args){
 
+        EmojiFeliz carafeliz = new EmojiFeliz();
+        EmojiTriste caratriste = new EmojiTriste();
 
+        carafeliz.setCara("feliz");
+        carafeliz.emocion();
+
+        caratriste.setCara("triste");
+        caratriste.emocion();
 
     }
 }
