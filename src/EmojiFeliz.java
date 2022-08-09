39f7c276008de148a9931adcb3fb903c26b7378e
Adrian-palacios19 <adrianspc1@gmail.com>
2022-08-09 17:04:01 -0500

    subiendo proyecto a github en la rama main

---
+++ src/EmojiFeliz.java
@@ -0,0 +1,19 @@
+public class  EmojiFeliz {
+
+    String cara = "feliz";
+
+    public void setCara(String cara) {
+        this.cara = cara;
+        System.out.println("emoji de cara feliz" + this.cara);
+    }
+
+    public String getCara(){
+        return  "emoji de cara feliz" + cara;
+    }
+
+    void emocion(){
+        System.out.println(" emocion = alegria");
+    }
+
+
+}
