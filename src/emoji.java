035ee9c2a69ffb7e42033f4875e3bc2fafc531c8
Adrian-palacios19 <adrianspc1@gmail.com>
2022-08-09 16:34:40 -0500

    subiendo proyecto a github en la rama main

---
+++ src/emoji.java
@@ -0,0 +1,18 @@
+public class emoji {
+        String cara;
+        String forma = "circulo";
+        String color = "amarillo";
+        String tamaño = "64px";
+
+        public void setCara(String cara) {
+            this.cara = cara;
+        }
+
+        void emociones(){
+            System.out.println("nada");
+        }
+
+
+
+
+}
