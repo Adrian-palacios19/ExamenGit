9ed0314cb2220a3dccb53ed028d20fa15aef9912
Adrian-palacios19 <adrianspc1@gmail.com>
2022-08-09 17:32:47 -0500

    subiendo segundo ejercicio a la rama main

---
+++ src/EmojiTriste.java
@@ -0,0 +1,19 @@
+public class EmojiTriste {
+
+    //creacion de la clase emoji triste
+    String cara = "triste";
+
+    public void setCara(String cara) {
+        this.cara = cara;
+        System.out.println("emoji de cara" + this.cara);
+    }
+
+    public String getCara(){
+        return  "emoji de cara" + cara;
+    }
+
+    void emocion(){
+        System.out.println("emocion = tristeza");
+    }
+
+}
